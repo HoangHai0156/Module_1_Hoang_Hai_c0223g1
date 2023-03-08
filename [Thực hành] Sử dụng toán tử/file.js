56954a113576815347d064a9c579c008a23175cd
HoangHai0156 <hoanghai41099@gmail.com>
2023-03-08 14:37:42 +0700

    08/03/23

---
+++ [Thực hành] Sử dụng toán tử/file.js
@@ -0,0 +1,6 @@
+inputWidth=prompt("Nhập chiều dài")
+inputHeight=prompt("Nhập chiều rộng")
+let width = parseInt(inputWidth)
+let height = parseInt(inputHeight)
+let area = width*height
+document.write("Area is :"+area)
