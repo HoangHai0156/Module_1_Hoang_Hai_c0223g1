24a96f10f4e395dee0091eaf2178f1ce4d8e0690
HoangHai0156 <hoanghai41099@gmail.com>
2023-03-08 14:19:41 +0700

    08/03/23

---
+++ [Thực hành] Khai báo và sử dụng biến/bai2.js
@@ -0,0 +1,4 @@
+let width=50
+let height=20
+let area=width*height
+document.write('Area = '+area)
---
+++ [Thực hành] Khai báo và sử dụng biến/bai3.js
@@ -0,0 +1,11 @@
+function boiSo(){
+    let a=prompt("Nhập giá trị a");
+    let b=prompt("Nhập giá trị b");
+    let r=a%b;
+    if (r=0){
+    text="a là bội số của b";
+    }else{
+    text="b là bội số của a";
+    }
+    alert(text);
+}
