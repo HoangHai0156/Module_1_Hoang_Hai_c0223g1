6372b821a412056472627e1fe80feab255c2f9c0
HoangHai0156 <hoanghai41099@gmail.com>
2023-03-09 14:20:31 +0700

    09/03/23

--- [Bài tập] Ứng dụng máy tính/script.js
+++ [Bài tập] Ứng dụng máy tính/script.js
@@ -1,24 +1,26 @@
+let aElement = document.getElementById("a");
+let bElement = document.getElementById("b");
 function evaluatecong(){
-    let numA = parseInt(document.getElementById("a").value);
-    let numB = parseInt(document.getElementById("b").value);
+    let numA = parseInt(aElement.value)
+    let numB = parseInt(bElement.value);
     let result=numA+numB;
-    document.getElementById("res").innerHTML=result;
+    document.getElementById("res").innerHTML="Kết quả của phép tính "+numA+"+"+numB+" là "+result;
 }
 function evaluatetru(){
-    let numA = parseInt(document.getElementById("a").value);
-    let numB = parseInt(document.getElementById("b").value);
+    let numA = parseInt(aElement.value)
+    let numB = parseInt(bElement.value);
     let result=numA-numB;
     document.getElementById("res").innerHTML="Kết quả của phép tính "+numA+"-"+numB+" là "+result;
 }
 function evaluatenhan(){
-    let numA = parseInt(document.getElementById("a").value);
-    let numB = parseInt(document.getElementById("b").value);
+    let numA = parseInt(aElement.value)
+    let numB = parseInt(bElement.value);
     let result=numA*numB;
     document.getElementById("res").innerHTML="Kết quả của phép tính "+numA+"x"+numB+" là "+result;
 }
 function evaluatechia(){
-    let numA = parseInt(document.getElementById("a").value);
-    let numB = parseInt(document.getElementById("b").value);
+    let numA = parseInt(aElement.value)
+    let numB = parseInt(bElement.value);
     let result=numA/numB;
     document.getElementById("res").innerHTML="Kết quả của phép tính "+numA+"/"+numB+" là "+result;
 }
