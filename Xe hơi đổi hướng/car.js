30e6dec9df3d5d1ea73521d14944b1d5b38276ee
HoangHai0156 <hoanghai41099@gmail.com>
2023-03-13 14:19:48 +0700

    13/03/23

---
+++ Xe hơi đổi hướng/car.js
@@ -0,0 +1,12 @@
+function carup(){
+    document.getElementById("cardirection").src="carup.jpg"
+}
+function cardown(){
+    document.getElementById("cardirection").src="cardown.jpg"
+}
+function carright(){
+    document.getElementById("cardirection").src="carright.jpg"
+}
+function carleft(){
+    document.getElementById("cardirection").src="carleft.jpg"
+}
