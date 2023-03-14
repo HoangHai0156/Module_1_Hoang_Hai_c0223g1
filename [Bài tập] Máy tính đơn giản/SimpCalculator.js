6acf0ecee3359836bcd167183b1444a08efcdd6f
HoangHai0156 <hoanghai41099@gmail.com>
2023-03-14 15:07:26 +0700

    14/03/23

---
+++ [Bài tập] Máy tính đơn giản/SimpCalculator.js
@@ -0,0 +1,46 @@
+var showBox = document.getElementById("inputBox");
+function insert1(){
+    showBox.value = showBox.value + 1;
+}
+function insert2(){
+    showBox.value = showBox.value + 2;
+}
+function insert3(){
+    showBox.value = showBox.value + 3;
+}
+function insert4(){
+    showBox.value = showBox.value + 4;
+}
+function insert5(){
+    showBox.value = showBox.value + 5;
+}
+function insert6(){
+    showBox.value = showBox.value + 6;
+}
+function insert7(){
+    showBox.value = showBox.value + 7;
+}
+function insert8(){
+    showBox.value = showBox.value + 8;
+}
+function insert9(){
+    showBox.value = showBox.value + 9;
+}
+function insert0(){
+    showBox.value = showBox.value + 0;
+}
+function insertPlus(){
+    showBox.value = showBox.value + "+";
+}
+function insertMinus(){
+    showBox.value = showBox.value + "-";
+}
+function insertX(){
+    showBox.value = showBox.value + "*";
+}
+function insertDivide(){
+    showBox.value = showBox.value + "/";
+}
+function output(){
+    showBox.value = eval(showBox.value);
+}
