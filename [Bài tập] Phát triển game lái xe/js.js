a558c85b6db6d874cbdeb0b08ad73b77f972f5a6
HoangHai0156 <hoanghai41099@gmail.com>
2023-03-27 14:43:09 +0700

    27/03/23

--- [Bài tập] Phát triển game lái xe/js.js
+++ [Bài tập] Phát triển game lái xe/js.js
@@ -1,4 +1,35 @@
-let ctx = document.getElementById("myCanvas").getContext("2d");
-var img = document.getElementById("myCar");
-console.log(ctx.innerWidth);
-ctx.drawImage(img,150,70,20,10);
+let cv = document.getElementById("myCanvas")
+console.log(cv.style.width);
+let ctx = cv.getContext("2d");
+var img1 = document.getElementById("yellowCar");
+var yellowCar;
+var myGameBoard;
+function startGame(){
+    myGameBoard = new gameBoard();
+    yellowCar = new carMold(img1,340,330,20,40);
+    console.log(myGameBoard.start());
+}
+
+let gameBoard = function(){
+    this.start = function(){
+        setInterval(updateBoard,20);
+    }
+    this.clear = function(){
+        ctx.clearRect(0,0,700,700);
+    }
+}
+let carMold = function(imagE,x,y,width,height){
+    this.img = imagE;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.update = function(){
+        ctx.drawImage(this.img,this.x,this.y,this.width,this.height);
+    }
+}
+let updateBoard = function(){
+    myGameBoard.clear();
+    // yellowCar.y -= 1;
+    yellowCar.update();
+}
